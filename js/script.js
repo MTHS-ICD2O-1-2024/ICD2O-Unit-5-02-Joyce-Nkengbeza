b5092c3b0b58f38904ff89739a263412d9810343
Joyce-Nkengbeza <Joyce.nkengbeza@stu.ocsb.ca>
2025-04-23 17:09:35 +0000

    positive

--- js/script.js
+++ js/script.js
@@ -1,22 +1,29 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Joyce Nkengbeza
-// Created on: Mar 2025
+// Created on: April 2025
 // This file contains the JS functions for index.html
 
 "use strict"
 
-function calculatearea() {
+'use strict'
+
+const positiveRandomNumber = Math.floor(Math.random() * 6) + 1
+const negativeRandomNumber = Math.floor(Math.random() * 6) * -1
+
+function calculate() {
   // input
-  const abase = parseInt(document.getElementById('a-base').value);
-  const bbase = parseInt(document.getElementById('b-base').value);
-  const height = parseInt(document.getElementById('height-length').value);
-    
-
+  const userInput = document.querySelector('input[name="kind-of-number"]:checked').value;
   // process
-    const trapezoidarea = [(abase + bbase) / 2] * height
-
-  // output
-  document.getElementById('area').innerHTML = 'area is:' + trapezoidarea + 'mm²'
+  if (userInput === "1") {
+    //output
+    document.getElementById('results').innerHTML =
+      '<p>The random number is:' + positiveRandomNumber + '</p>'
+  }
+  else {
+    // output
+    document.getElementById('results').innerHTML =
+      '<p>The random number is: ' + negativeRandomNumber + '</p>';
+  }
 }
 
